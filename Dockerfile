89294deea2f904d9e3d26c70b80839c3dd551ca0
chirag-memriya <chirag.memriya@matrixcomsec.com>
2023-05-08 16:20:08 +0530

    add jenkins file

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,32 @@
-FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build-stage
-ENV ASPNETCORE_URLS=http://+:80
+# FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build-stage
+# ENV ASPNETCORE_URLS=http://+:80
+# WORKDIR /app
+# COPY . ./
+# EXPOSE 80
+# ENTRYPOINT ["dotnet", "./PizzaApi.dll"]
+
+# # Use the official .NET Core SDK image as the base image
+FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build-env
+
+# # Set the working directory to /app
 WORKDIR /app
+
+# Copy the csproj file and restore any dependencies
+COPY *.csproj ./
+RUN dotnet restore
+
+# Copy the remaining source code and build the app
 COPY . ./
+RUN dotnet publish -c Release -o out
+
+# Create a new image from the base image
+FROM mcr.microsoft.com/dotnet/aspnet:6.0
+WORKDIR /app
+COPY --from=build-env /app/out .
+
+# Expose port 80 for the app to listen on
 EXPOSE 80
-ENTRYPOINT ["dotnet", "./PizzaApi.dll"]
+
+# Start the app
+ENTRYPOINT ["dotnet", "PizzaApi.dll"]
+
---
+++ ok.groovy
@@ -0,0 +1,62 @@
+pipeline {
+    agent any
+    environment {
+        APP_NAME = "my-web-dotnet-app"
+        APP_PORT = "5000"
+        IMAGE_TAG = "latest"
+        DOCKER_REGISTRY="memariyachirag126"
+        
+    }
+    stages {
+        stage('Build') {
+            steps {
+                
+                // Build the project
+                sh 'dotnet build'
+            }
+        }
+        stage('test') {
+            steps {
+                
+                // test the project
+                sh 'dotnet test'
+            }
+        }
+        stage('pulbish'){
+            steps{
+                // Publish the project
+                sh 'dotnet publish -c Release -o ./publish'
+            }
+        }
+        stage('Docker Build') {
+            steps {
+                // Build Docker image
+                script {
+                    docker.build("${DOCKER_REGISTRY}/${APP_NAME}:${IMAGE_TAG}", "--build-arg APP_PORT=${APP_PORT} .")
+                }
+            }
+        }
+        stage('Docker Login') {
+            steps {
+                // Log in to Docker registry
+                withCredentials([usernamePassword(credentialsId: 'docker-token', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
+                    sh "docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD} ${DOCKER_REGISTRY}"
+                }
+            }
+        }
+//         stage('Docker Push') {
+//             steps {
+                
+//                 }
+//             }
+//         }
+    }
+}
+
+
+/*
+
+jdk : /Library/Java/JavaVirtualMachines/jdk-17.jdk/Contents/Home/bin
+    docker-token
+
+*/
